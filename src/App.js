64c85a5f6fc1fd7b3d7bd81b72f11a9f7abb8eed
projectkingz <44386193+projectkingz@users.noreply.github.com>
2023-01-18 12:54:31 +0000

    Porfolio Website v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import NavBar from "./Components/Navbar/Navbar"
+import Hero from "./Components/Hero/Hero"
+import Skills from "./Components/Skills/Skills"
+import Projects from "./Components/Projects/Projects"
+import Contact from "./Components/Contact/Contact"
+import Newsletter from "./Components/Newsletter/Newsletter"
+import Footer from "./Components/Footer/Footer"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Hero />
+      <Skills />
+      <Projects />
+      <Contact />
+
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
